1dae2d3e5703ff3aa11e692043a33fb2b0cca86d
Benjamin Csonto <benjamin.csonto@lunatech.nl>
2026-05-28 01:10:55 +0200

    Setup node state skeleton and task-based execution

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 This project localizes ESP32-C3 devices using RSSI with the ESP-NOW protocol.
 
-Run with
-```cmd
+## Commands
+```bash
+# Run code
 cargo run --release
+# Set board ID
+ID=1 cargo run --release
+# Attach terminal to running board
+espflash monitor
+# Make sure to format your code
+cargo clippy
 ```
---
+++ src/state.rs
@@ -0,0 +1,26 @@
+use hashbrown::HashMap;
+
+
+const RSSI_SCALE: f32 = 190.0;
+
+
+#[derive(Debug)]
+pub struct NodeState {
+    pub neighbours: HashMap<[u8; 6], f32>,
+}
+
+impl NodeState {
+
+    pub fn default() -> NodeState {
+        NodeState {
+            neighbours: HashMap::new(),
+        }
+    }
+
+    pub fn update(&mut self, src_address: [u8; 6], rssi:  i32) {
+        // TODO: update with proper estimation
+        let distance = rssi as f32 / RSSI_SCALE;
+        self.neighbours.insert(src_address, distance);
+    }
+
+}
